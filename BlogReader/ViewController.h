c0dd185c839f5c1b4429c9ef931f242eac30b6fb
Daniel Ebrahim <danielebrahim@Daniels-MacBook-Air.local>
2014-07-05 17:32:53 -0400

    Initial Commit

---
+++ BlogReader/ViewController.h
@@ -0,0 +1,13 @@
+//
+//  ViewController.h
+//  BlogReader
+//
+//  Created by Daniel Ebrahim on 7/5/14.
+//  Copyright (c) 2014 DZCompany. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface ViewController : UIViewController
+
+@end
---
+++ BlogReader/ViewController.m
@@ -0,0 +1,29 @@
+//
+//  ViewController.m
+//  BlogReader
+//
+//  Created by Daniel Ebrahim on 7/5/14.
+//  Copyright (c) 2014 DZCompany. All rights reserved.
+//
+
+#import "ViewController.h"
+
+@interface ViewController ()
+
+@end
+
+@implementation ViewController
+
+- (void)viewDidLoad
+{
+    [super viewDidLoad];
+	// Do any additional setup after loading the view, typically from a nib.
+}
+
+- (void)didReceiveMemoryWarning
+{
+    [super didReceiveMemoryWarning];
+    // Dispose of any resources that can be recreated.
+}
+
+@end
